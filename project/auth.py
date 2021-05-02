38e6fec239346a4177a44415dce52a9a443c2607
Catch-XXII <cgultekink@gmail.com>
2021-05-02 15:07:02 +0300

    Update auth.py

--- project/auth.py
+++ project/auth.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
 from .models import User
 from . import db
-from flask_login import login_user, logout_user, login_required
 
 auth = Blueprint('auth', __name__)
 
@@ -50,4 +50,3 @@ def signup_post():
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
-
