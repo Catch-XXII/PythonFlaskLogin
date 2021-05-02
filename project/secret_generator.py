943225def2f71f276157ddfd11ce77c7e87ae4c1
Catch-XXII <cgultekink@gmail.com>
2021-05-02 20:10:15 +0300

    Update secret_generator.py

--- project/secret_generator.py
+++ project/secret_generator.py
@@ -5,5 +5,7 @@ alphabet = string.ascii_letters + string.digits
 
 def skg() -> str:
     password = "".join(secrets.choice(alphabet) for _ in range(16))
-    if (any(c.islower() for c in password) and any(c.isupper() for c in password) and sum(c.isdigit() for c in password) >= 3):
+    if (any(c.islower() for c in password) and 
+        any(c.isupper() for c in password) and 
+        sum(c.isdigit() for c in password) >= 3):
         return password
