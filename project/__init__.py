6ed58a680cfe72f44760ed941547bd2450544934
Catch-XXII <cgultekink@gmail.com>
2021-05-02 20:10:35 +0300

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask import Flask
 from secret_generator import skg
+from flask import Flask
+
 
 db = SQLAlchemy()
 
